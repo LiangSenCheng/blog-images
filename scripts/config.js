247a411b9e5ac0a0602cdf30b880023c85f4803f
liangsc <liangsc@300624.cn>
2021-12-22 17:11:09 +0800

    test: script test1;

---
+++ scripts/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+  GIT_USERNAME: "Auto-Script", // git.userName
+  GIT_EMAIL: "blog@images.sync", // git.email
+  GIT_REF: "main", // git.ref, 即本地的分支
+  GIT_REMOTE: "origin", // git.remote
+  GIT_DIR: "", // .git的目录路径,若SYS_DIR已经包含.git目录,则GIT_DIR设置为空;
+  IMG_DIR: "images", // 待遍历文件的目录
+  SYS_DIR: "C:\\Users\\ws\\Desktop\\Codes\\blog-images", // 根目录文件路径 若为空时,默认是执行文件所在目录
+  REMOTE_URL: "https://github.com/LiangSenCheng/blog-images.git", // github地址
+  SOURCE_URL: "https://cdn.jsdelivr.net/gh/LiangSenCheng/blog-images",
+};
+
+module.exports = { CONFIG };
---
+++ scripts/generate-link.js
@@ -0,0 +1,71 @@
+const fs = require("fs");
+const path = require("path");
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const { fileDisplay } = require("./scripts/file-dir");
+
+const { CONFIG } = require("./config");
+
+/**
+ * 更新文件
+ */
+function updateText(rootFolder, fileList) {
+  // 文件路径
+  const listTxtFilePath = path.join(rootFolder, "list.txt");
+  const listData = fileList.reverse().map((item) => `${CONFIG.SOURCE_URL}/${CONFIG.IMG_DIR}${item}`);
+
+  // 删除旧文件
+  fs.rmSync(listTxtFilePath, { force: true });
+  // 更新txt
+  fs.writeFileSync(listTxtFilePath, `${listData.join("\n")}`, "utf8");
+}
+
+/**
+ * 更新json
+ */
+function updateJson(rootFolder, fileList) {
+  // 文件路径
+  const listJsonFilePath = path.join(rootFolder, "list.json");
+  const listData = fileList.reverse().map((item) => `${CONFIG.SOURCE_URL}/${CONFIG.IMG_DIR}${item}`);
+  const listObj = {
+    list: listData,
+    timestamp: new Date().getTime(),
+    updateTime: dayjs(res.data.timestamp).tz("Asia/Shanghai").format("YYYY-MM-DD HH:MM:ss"),
+  };
+  // 删除旧文件
+  fs.rmSync(listJsonFilePath, { force: true });
+  fs.writeFileSync(listJsonFilePath, JSON.stringify(listObj));
+}
+
+/**
+ * 更新readme
+ */
+function updateReadme(rootFolder) {
+  // 文件路径
+  const readmeFilePath = path.join(rootFolder, "README.md");
+  const readmeStr = `# blog-images \n\n* ${CONFIG.SOURCE_URL}/${CONFIG.IMG_DIR
+    }/ \n\n* ${CONFIG.SOURCE_URL}/list.json \n\n* ${CONFIG.SOURCE_URL
+    }/list.txt \n\n${moment().format("YYYY-MM-DD HH:mm:ss")}`;
+  // 删除旧文件
+  fs.rmSync(readmeFilePath, { force: true });
+  fs.writeFileSync(readmeFilePath, readmeStr);
+}
+
+async function main() {
+  // 获取指定目录下的所有文件路径
+  const rootFolder = CONFIG.SYS_DIR ? CONFIG.SYS_DIR : process.cwd();
+  // 路径列表
+  const fileList = fileDisplay(rootFolder);
+  // 更新txt
+  updateText(rootFolder, fileList);
+  // 更新json
+  updateJson(rootFolder, fileList);
+  // 更新readme
+  updateReadme(rootFolder);
+}
+
+main();
