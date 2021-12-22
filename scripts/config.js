768a5f608008d08b8b07b14be1e089a2ee3329ee
liangsc <liangsc@300624.cn>
2021-12-22 17:21:10 +0800

    test1

--- scripts/config.js
+++ scripts/config.js
@@ -3,8 +3,7 @@ const CONFIG = {
   GIT_REMOTE: "origin", // git.remote
   GIT_DIR: "", // .git的目录路径,若SYS_DIR已经包含.git目录,则GIT_DIR设置为空;
   IMG_DIR: "images", // 待遍历文件的目录
-  SYS_DIR: "C:\\Users\\ws\\Desktop\\Codes\\blog-images", // 根目录文件路径 若为空时,默认是执行文件所在目录
-  REMOTE_URL: "https://github.com/LiangSenCheng/blog-images.git", // github地址
+  SYS_DIR: "/", // 根目录文件路径 若为空时,默认是执行文件所在目录
   SOURCE_URL: "https://cdn.jsdelivr.net/gh/LiangSenCheng/blog-images",
 };
 
